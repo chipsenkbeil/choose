cd178eb8c1ba02b3efb69a96bb03fa8bc807c005
Steven Degutis <sbdegutis@gmail.com>
2015-03-18 15:29:14 -0500

    Initial Commit

---
+++ choose/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  choose
+//
+//  Created by Steven Degutis on 3/18/15.
+//  Copyright (c) 2015 Tiny Robot Software. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
