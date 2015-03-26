b7eb398ba306515c066fdd6b99c7bf49e4798582
Steven Degutis <sbdegutis@gmail.com>
2015-03-26 12:09:47 -0500

    Much simpler way of choosing fonts (assuming it works).

--- main.swift
+++ main.swift
@@ -6,32 +6,30 @@ var useIndexes = false
 var highlightColor = colorFromHex("0000FF")
 var numRows = 10
 var percentWidth: Double? = nil
-var queryFont: NSFont
+var queryFont = NSFont(name: "Menlo", size: 26.0)!
 
 func showVersion() {
-    println(NSBundle.mainBundle().infoDictionary!["CFBundleVersion"]);
+    println(NSBundle.mainBundle().infoDictionary!["CFBundleVersion"])
     exit(0)
 }
 
-let defaultQueryFont = NSFont(name: "Menlo", size: 26.0)!
-var fontName = defaultQueryFont.fontName
-var fontSize = defaultQueryFont.pointSize
+func chooseFont(name: String = queryFont.fontName, size: Double = Double(queryFont.pointSize)) {
+    queryFont = NSFont(name: name, size: CGFloat(size)) ?? queryFont
+}
 
 CommandLine.parse(
     usage: { "usage: \($0) [-i] [-v] [-n rows=10] [-w widthpercent=auto] [-f fontname=Menlo] [-s fontsize=26] [-c highlight=0000FF]" },
     flags: [
         "i": .V({ useIndexes = true }),
-        "f": .S({ fontName = $0 }),
+        "f": .S({ chooseFont(name: $0) }),
         "c": .S({ highlightColor = colorFromHex($0) }),
-        "s": .D({ fontSize = CGFloat($0) }),
+        "s": .D({ chooseFont(size: $0) }),
         "n": .I({ numRows = $0 }),
         "w": .D({ percentWidth = $0 }),
         "v": .V({ showVersion() }),
         "h": .Usage,
     ],
     done: { args in
-        queryFont = NSFont(name: fontName, size: fontSize) ?? defaultQueryFont
-        
         chooser.ensureHasItems()
         
         app.setActivationPolicy(.Accessory)
