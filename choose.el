0a8cf099758e629453059d969eae555f9fabc749
Steven Degutis <sdegutis@users.noreply.github.com>
2015-09-12 23:12:55 -0500

    Create choose.el

---
+++ choose.el
@@ -0,0 +1,14 @@
+;; better find-file-in-repository
+(defun choose/find-file-in-git-repo ()
+  (interactive)
+  (require 's)
+  (let ((root-dir (magit-toplevel default-directory)))
+    (if root-dir
+        (let ((default-directory root-dir))
+          (let ((f (s-trim
+                    (shell-command-to-string
+                     "git ls-files -co --exclude-standard | choose"))))
+            (unless (string= "" f)
+              (find-file f))))
+      (call-interactively 'find-file))))
+(global-set-key (kbd "C-x f") 'choose/find-file-in-git-repo)
